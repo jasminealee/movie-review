b07a2dcd17a958116822b8f1f5bee5524790b28a
Jasmine Lee <jasmine.al1722@gmail.com>
2019-04-12 08:27:22 -0700

    add adding attempting to add retrieving parameters

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -3,19 +3,21 @@ import { Movie } from '../movie.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
-export class MovieDetailComponent {
-  constructor(private router: Router){}
+export class MovieDetailComponent implements OnInit {
+  movieId: number = null;
+  constructor(private route: ActivatedRouter, private location: Location) {}
+
+  ngOnInit() {
+  }
   movies: Movie[] = [
     new Movie(["The best movie ever!"], 1)
   ];
   goToDetailPage(clickedMovie: Movie) {
-    this.router.navigate(['movies', clickedMovie.id]);
-};
+    this.route.navigate(['movies', clickedMovie.id]);
+  };
 }
